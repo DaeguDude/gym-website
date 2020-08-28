730605b9be858a26b0866aca2f9b9d873fee1029
Daegudude <k3hppk@gmail.com>
2020-08-28 13:12:10 +0900

    0828 done

--- js/display/display.js
+++ js/display/display.js
@@ -1,3 +1,5 @@
+import { Student } from '../student/student.js';
+
 const displayController = (() => {
   // 확인버튼
   let numSubmitBtn = document.getElementById('num-submit-btn');
@@ -5,17 +7,20 @@ const displayController = (() => {
   // 회원번호가 나타나는 input 요소
   let studentNum = document.getElementById('student-num');
 
-  // 확인을 눌렀을 시, input#student-num의 값을 반환해줌
+  // 확인을 눌렀을 시, input#student-num의 값을 반환해주는 이벤트를 활성화시키는 함수
   const enableNumSubmitBtn = () => {
     let studentContact = '';
     numSubmitBtn.addEventListener('click', (event) => {
       // 변수에 회원이 입력한 전화번호 값을 넣어줌
       studentContact = studentNum.value;
-      console.log(studentContact);
-
       // 회원 번호입력란(input#student-num) 초기화
       studentNum.value = '';
 
+      if(Student.checkContact(studentContact)) {
+        console.log('he exists bro');
+      } else {
+        console.log(`${studentContact} is not there!`);
+      }
     })
   }
 
@@ -41,3 +46,12 @@ const displayController = (() => {
 
 displayController.enableNumSubmitBtn();
 displayController.enableNumBtns();
+
+
+Student.makeStudent('김상학', '남자', '29', '01049931290', '대구광역시 달서구 도원동 311동 506호');
+Student.makeStudent('박경환', '남자', '33', '01038819804', '대구광역시 달서구 성서');
+Student.makeStudent('윤길상', '남자', '33', '01077461153', '대구광역시 달서구 옥포');
+Student.makeStudent('이승철', '남자', '28', '01068880063', '대구광역시 달서구 상인동');
+Student.makeStudent('오득원', '남자', '29', '01041204839', '대구광역시 달서구 월배 2차 아이파크');
+
+console.log(Student.getAllStudents());
--- js/student/student.js
+++ js/student/student.js
@@ -1,14 +1,56 @@
-// 관원들을 생성해주는 함수
-const studentFactory = (name, gender, age, contact, address) => {
-  
-  return {
-    name,
-    gender,
-    age,
-    contact,
-    address
-  }
-}
+// 관원과 관련된 일을 해주는 모듈
+const Student = (() => {
+  // 관원들을 담은 배열
+  const students = [];
 
-let student1 = studentFactory('Sanghak', '남자', '29', '010-4993-1290', '대구');
-console.log(student1);
+  // 관원들을 생성해주는 함수
+  const makeStudent = (name, gender, age, contact, address) => {
+    // 새로운 관원을 배열(students)에 추가해줌 
+    students.push({name, gender, age, contact, address});
+  }
+
+  // 모든 관원 오브젝트를 반환해주는 함수
+  const getAllStudents = () => {
+    return students;
+  }
+
+  // 모든 학생 전화번호를 반환해주는 함수, 배열로 반환함
+  const getAllStudentsContact = () => {
+    let contactList = [];
+    students.forEach(student => {
+      contactList.push(student.contact);
+    })
+
+    return contactList;
+  }
+
+  // 입력한 학생의 번호가 있는지 검색해주는 함수, 있으면 true 없으면 false 반환
+  const checkContact = (contact) => {
+    // 변수에 전화번호 목록을 다 담아줌
+    let contactList = getAllStudentsContact();
+    // 전화번호가 있으면 true 없으면 false
+    let isThere = false;
+
+    // 관원이 입력한 전화번호가 전화번호부에 있는지 검색 있으면 true 반환
+    contactList.forEach(phoneNum => {
+      if(contact === phoneNum) {
+        isThere = true;
+        return isThere;
+      }
+    })
+
+    // 없다면 false 반환
+    return isThere;
+  }
+
+  return {
+    makeStudent,
+    getAllStudents,
+    getAllStudentsContact,
+    checkContact
+  }
+})();
+
+export { Student };
+
+
